7fbc9692511206fbe9a35308e59d55a8337f17c6
x1598 <mf@x1598.at>
2010-06-09 07:22:37 +0200

    added header file for binary search tree implementation

---
+++ inc/bintree.h
@@ -0,0 +1,10 @@
+struct bt_node {
+	void *datą;
+	struct bt_node *left, *right;
+};
+
+struct bt_node* bt_new_node(void *data);
+void bt_free_node(struct bt_node *node);
+struct bt_node** bt_search(struct bt_node **tree,int (*compare)(const void *, const void *), void *data);
+void bt_insert(struct bt_node **tree,int (*compare)(const void *, const void *), void *data);
+void bt_delete(struct bt_node **node);
